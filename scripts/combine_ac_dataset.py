7c7c458bcfe2141b606d62d4f91dc313211dcccc
pjmcc2 <pjmcc2@gmail.com>
2024-08-22 15:22:20 -0700

    found bug (not fixed) with AC dataset

--- scripts/combine_ac_dataset.py
+++ scripts/combine_ac_dataset.py
@@ -1,16 +1,14 @@
 import pandas as pd
-
-import pandas as pd
 import re
 import os
 
 def merge_files_to_dataframe(directory):
     # Dictionary to hold dataframes for each X value
     data_dict = {}
-    
+
     # Regular expression pattern to match files of the form "SubXResults_pY.txt"
     pattern = re.compile(r"Sub(\d{1,2})Results_p(\d).txt")
-    
+
     # Iterate through all files in the specified directory
     for filename in os.listdir(directory):
         # Check if the file matches the pattern
@@ -19,49 +17,29 @@ def merge_files_to_dataframe(directory):
             # Extract X and Y values from the filename
             x_value = match.group(1)
             y_value = match.group(2)
-            
+
             # Construct the full path of the file
             file_path = os.path.join(directory, filename)
-            
+
             # Read the file into a DataFrame
-            df = pd.read_csv(file_path, delimiter='\t',names=["page_number","sound_file",f"rating_{x_value}"])  # Adjust delimiter if necessary
+            df = pd.read_csv(file_path, delimiter=None,names=["page_number","sound_file",f"rating_{x_value}"])  # Adjust delimiter if necessary
             df = df.drop("page_number",axis=1).sort_values("sound_file").set_index("sound_file")
 
-            
+
             # Append or create a new entry in the dictionary for this X value
             if x_value not in data_dict:
                 data_dict[x_value] = df
             else:
                 data_dict[x_value] = pd.concat([data_dict[x_value], df], ignore_index=True)
-    
+
     # Combine all DataFrames for each X value into a single DataFrame
-    combined_df = pd.concat(data_dict.values(),axis=1,sort=False).reset_index()
-    combined_df.rename(columns = {'index':'sound_file'})
-    
+    combined_df = pd.concat(data_dict.values(),axis=1,sort=False)
+    combined_df = combined_df.reset_index()
+    combined_df = combined_df.rename(columns = {'index':'sound_file'})
+
     return combined_df
+       
 
-
-def aggregate_data(df,kind='mean'):
-    kind_vals = ['vote','mean','greater_median',None]
-    if kind not in kind_vals:
-        raise ValueError(f"{kind} not in {kind_vals}")
-
-    if kind =='mean':
-        return round(df.mean(axis=1)/100)
-    else:
-        encoded = mean_encode(df)
-        if kind == 'vote':
-            return [round(sum(row)/len(row)) for _,row in encoded.iterrows()] # TODO Fix
-        
-
-
-def mean_encode(df):
-    means = df.mean(axis=0)
-    vals = {}
-    for i,tup in enumerate(df.items()):
-        name,series = tup
-        vals[name] = series > means[i]
-    return pd.DataFrame(vals)
 
 
 #  a directory and its name
@@ -69,17 +47,18 @@ results_directories = ["/nfs/guille/eecs_research/soundbendor/datasets/sounds_an
                        "/nfs/guille/eecs_research/soundbendor/datasets/sounds_and_noise/AudioCommonsTimbre/TimbralModels_v0.2_Development/Brightness/Brightness ratings/Listening tests/Results",
                        "/nfs/guille/eecs_research/soundbendor/datasets/sounds_and_noise/AudioCommonsTimbre/TimbralModels_v0.2_Development/Depth/Depth ratings/Listening tests/Results/",
                        "/nfs/guille/eecs_research/soundbendor/datasets/sounds_and_noise/AudioCommonsTimbre/TimbralModels_v0.2_Development/Hardness/Hardness ratings/Listening tests/Results",
-                       "/nfs/guille/eecs_research/soundbendor/datasets/sounds_and_noise/AudioCommonsTimbre/TimbralModels_v0.2_Development/Reverb/Reverb ratings/Listening tests/Results",
+                       "/nfs/guille/eecs_research/soundbendor/datasets/sounds_and_noise/AudioCommonsTimbre/TimbralModels_v0.2_Development/Roughness/Roughness ratings/Listening tests/Results",
                        "/nfs/guille/eecs_research/soundbendor/datasets/sounds_and_noise/AudioCommonsTimbre/TimbralModels_v0.2_Development/Sharpness/Sharpness ratings/Listening tests/Results",
                        "/nfs/guille/eecs_research/soundbendor/datasets/sounds_and_noise/AudioCommonsTimbre/TimbralModels_v0.2_Development/Warmth/Warmth ratings/Listening tests/Results",]
 
-dir_labels = ["Boominess","Brightness","Depth","Hardness","Reverb","Roughness","Sharpness","Warmth"]
-# for each results directory, combine parts into one file, then average across columns
-mean_dict = {} 
+dir_labels = ["Boominess","Brightness","Depth","Hardness","Roughness","Sharpness","Warmth"]
+
+DATA_PATH = "/nfs/guille/eecs_research/soundbendor/mccabepe/timbre_tags/data"
 for name,dir in zip(dir_labels,results_directories):
     combined_df = merge_files_to_dataframe(dir)
-    #calculate median of columns
-    #determine aggregation strategies: OR, mean, Vote, keep all, etc.
+    print(combined_df.head())
+    write_file = f"ACTD_{name}.pickle"
+    combined_df.to_pickle(os.path.join(DATA_PATH,write_file))
     
 
     
