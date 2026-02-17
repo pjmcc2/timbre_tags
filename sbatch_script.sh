fd6704873356875c258074e905f5327f0d1c7e02
Peter McCabe <pjmcc2@gmail.com>
2026-02-17 12:50:20 -0800

    more changes

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -2,12 +2,12 @@
 #
 #SBATCH --partition=soundbendor
 #SBATCH --account=soundbendor
-#SBATCH --job-name=no_audio_sig
+#SBATCH --job-name=embed_wav
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-gpu=6G
+#SBATCH --mem-per-gpu=15G
 #SBATCH --time=23:59:59
-#SBATCH -o scripts/gen.out
-#SBATCH -e scripts/gen.err
+#SBATCH -o scripts/sbatch.out
+#SBATCH -e scripts/sbatch.err
 
 module load slurm
 source env/bin/activate
