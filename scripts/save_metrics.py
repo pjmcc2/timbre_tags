9af646a1c8882102850f4c8b1ffe2ab2c1d29655
Peter McCabe <pjmcc2@gmail.com>
2026-02-18 22:53:46 -0800

    new timbre

--- scripts/save_metrics.py
+++ scripts/save_metrics.py
@@ -1,3 +1,23 @@
 # TODO: calc metrics for all augmentations on clotho+sb.
 # Calc all unsupervised distances on all language datasets and Clotho+sb audio. 
 # ??? calc all unsupervised distances on language and val datasets? 
+
+from src.calc_metrics import calc_dist_metrics,calc_rep_metrics
+from src.augment_data import _project
+
+def calc_metrics(text_data,audio_data,augment_method,noisy=False):
+
+    
+    functions = [
+
+    ]
+
+
+
+    pass #TODO
+
+
+
+def main(out_path):
+
+    pass
